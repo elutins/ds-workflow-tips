a766b1d10b2be854266b4c7ca33df153b29020d3
Evan Lutins <elutins@pinpoint.com>
2020-04-20 08:56:48 -0400

    configuring logger

---
+++ src/logger_setup.py
@@ -0,0 +1,52 @@
+import logging
+import sys
+import structlog
+import logging.config
+
+
+def create_logger():
+    return structlog.get_logger()
+
+
+def initialize(level="INFO"):
+    log_levels = {
+        "DEBUG": logging.DEBUG,
+        "INFO": logging.INFO,
+        "WARNING": logging.WARNING,
+        "WARN": logging.WARNING,
+        "ERROR": logging.ERROR,
+        "CRITICAL": logging.CRITICAL,
+    }
+
+    timestamper = structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S")
+    pre_chain = [
+        # Add the log level and a timestamp to the event_dict if the log entry
+        # is not from structlog.
+        structlog.stdlib.add_log_level,
+        timestamper,
+    ]
+
+    logging.config.dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "colored": {
+                    "()": structlog.stdlib.ProcessorFormatter,
+                    "processor": structlog.dev.ConsoleRenderer(colors=True),
+                    "foreign_pre_chain": pre_chain,
+                },
+            },
+            "handlers": {
+                "default": {"level": log_levels[level], "class": "logging.StreamHandler", "formatter": "colored",},        
+                "loggers": {"": {"handlers": ["default", "file"], "level": log_levels[level], "propagate": True,},},
+            }
+        }
+    )  
+            
+    structlog.configure(
+            context_class=dict,
+            logger_factory=structlog.stdlib.LoggerFactory(),
+            wrapper_class=structlog.stdlib.BoundLogger,
+            cache_logger_on_first_use=True,
+        )
